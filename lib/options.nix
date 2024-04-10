005fef9734547a30ef3b8c6615252fe34f9ef31a
Jakob Wenzel <wenzel@rs.tu-darmstadt.de>
2024-04-10 19:07:00 +0200

    remove environment stuff

--- lib/options.nix
+++ lib/options.nix
@@ -19,29 +19,15 @@ let
 in
 {
   options = {
-    nix.generateNixPathFromInputs = mkFalseOption "Generate NIX_PATH from available inputs.";
     nix.generateRegistryFromInputs = mkFalseOption "Generate Nix registry from available inputs.";
-    nix.linkInputs = mkFalseOption "Symlink inputs to /etc/nix/inputs.";
   };
 
   config = {
-    assertions = [
-      {
-        assertion = !cfg.generateNixPathFromInputs || cfg.linkInputs;
-        message = "When using 'nix.generateNixPathFromInputs' please make sure to set 'nix.linkInputs = true'";
-      }
-    ];
 
     nix.registry =
       if cfg.generateRegistryFromInputs
       then nixRegistry
       else { self.flake = flakes.self; };
-
-    environment.etc = mkIf (cfg.linkInputs || cfg.generateNixPathFromInputs) (mapAttrs'
-      (name: value: { name = "nix/inputs/${name}"; value = { source = value.outPath; }; })
-      inputs);
-
-    nix.nixPath = mkIf cfg.generateNixPathFromInputs [ "/etc/nix/inputs" ];
   };
 }
 
